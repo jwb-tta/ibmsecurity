3cb54948163742ba081b248ac8c5d035b37afeb7
Yartsev, Ivan (EXTERN: TIMETOACT) <extern.ivan.yartsev@audi.de>
2019-05-13 14:23:27 +0200

    +subject and issuer

--- ibmsecurity/isam/base/ssl_certificates/all_certificates.py
+++ ibmsecurity/isam/base/ssl_certificates/all_certificates.py
@@ -23,13 +23,30 @@ def get_all_certificates (isamAppliance, check_mode=False, force=False):
 		pcerts=pcert_obj['data']
 		for pcert in pcerts:
 			cert_epoch = int(pcert['notafter_epoch'])
-			certs.append({"db_id":db['id'],"cert_id":pcert['id'],"type":"personal","exp_epoch":pcert['notafter_epoch'],"exp_date":pcert['notafter'],"expired":cert_epoch < epoch_time})
+			certs.append({
+						"db_id":db['id'],
+						"cert_id":pcert['id'],
+						"issuer":pcert['issuer'],
+						"subject":pcert['subject'],						
+						"type":"personal",
+						"exp_epoch":pcert['notafter_epoch'],
+						"exp_date":pcert['notafter'],
+						"expired":cert_epoch < epoch_time
+						})
 
 		scert_obj=isamAppliance.invoke_get("Retrieve signer certificates", "{0}/{1}/signer_cert".format(uri,db['id']))
 		scerts=scert_obj['data']
 		for scert in scerts:
 			cert_epoch = int(scert['notafter_epoch'])
-			certs.append({"db_id":db['id'],"cert_id":scert['id'],"type":"signer","exp_epoch":scert['notafter_epoch'],"exp_date":scert['notafter'],"expired":cert_epoch < epoch_time})
+			certs.append({
+						"db_id":db['id'],
+						"cert_id":scert['id'],
+						"issuer":scert['issuer'],						
+						"subject":scert['subject'],
+						"type":"signer",
+						"exp_epoch":scert['notafter_epoch'],
+						"exp_date":scert['notafter'],
+						"expired":cert_epoch < epoch_time})
 
 		
 	return_obj = isamAppliance.create_return_object()
